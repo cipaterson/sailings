f7e896e501ced6c7fbe57094e90888be0a8123b2
Chris Paterson <cipaterson@proton.me>
2026-06-01 16:09:05 +1000

    Added Charter State colors to sailings views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def can?(role)
-     current_user&.has_role?(role.to_s)
-   end
+    current_user&.has_role?(role.to_s)
+  end
+
+  def charter_calendar_class(sailing)
+    return nil unless sailing.charterer.present?
+    { "TBC" => "charter-tbc", "Confirmed" => "charter-confirmed",
+      "Outstanding" => "charter-outstanding", "Paid" => "charter-paid" }[sailing.charter_state]
+  end
 end
